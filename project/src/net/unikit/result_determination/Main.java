818734b12d9047b8d890f7c1402fe1e5c89a2343
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-10 19:42:24 +0100

    Added empty project with libraries.

---
+++ project/src/net/unikit/result_determination/Main.java
@@ -0,0 +1,10 @@
+package net.unikit.result_determination;
+
+/**
+ * Created by Andreas Berks on 10.11.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
